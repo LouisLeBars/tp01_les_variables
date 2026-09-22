936b99a7068dd6413ecdf6affcb5b3e1b9cd5e9b
llebarsmartin <llebarsmartin@350028R-F115X10.lise.kreizenn.bzh>
2026-09-22 12:44:03 +0200

    modification parti 4 debut

--- src/les_variable_en_java/Les_variable_en_java.java
+++ src/les_variable_en_java/Les_variable_en_java.java
@@ -3,19 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package les_variable_en_java;
-
+import java.util.Scanner;
 /**
  *
  * @author llebarsmartin
  */
 public class Les_variable_en_java {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("ceci est un message");
-       System.out.println("sur plusieurligne");
-    }
+        Scanner sc = new Scanner(System.in);
+          int x = 0;
+          int y = 0;
+          System.out.print("donner un nombre pour x et y ? : ");
+          x = sc.nextInt();
+          y = sc.nextInt();          
+            System.out.print
+        
+                  }                }
     
-}
+
+    
+   
+    
+
