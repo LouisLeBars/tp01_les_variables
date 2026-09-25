e7a59cf91bb845c7623758f38d3af201b31f805b
llebarsmartin <llebarsmartin@350028R-F115X10.lise.kreizenn.bzh>
2026-09-25 15:04:38 +0200

    fin exercice partie 5

--- src/les_variable_en_java/Les_variable_en_java.java
+++ src/les_variable_en_java/Les_variable_en_java.java
@@ -13,23 +13,34 @@ public class Les_variable_en_java {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+ /*       Scanner sc = new Scanner(System.in);
           int x = 0;
           int y = 0;
           int z = 0;
-          System.out.print("saisir x ? : ");/** l'utilisateur rentre une valaur pour x et y
-           */
+          System.out.print("saisir x ? : ");// l'utilisateur rentre une valaur pour x et y
           x = sc.nextInt();
           System.out.print("saisir y ? : ");
           y = sc.nextInt();  
-          System.out.println("avant permutation x="+x+ " et y="+y);/**on echange les valeurs x et y en passant par une variable z pur ne pas perdre la valeur de x 
-           */
+          System.out.println("avant permutation x="+x+ " et y="+y);//on echange les valeurs x et y en passant par une variable z pur ne pas perdre la valeur de x 
+           
            z = x;
            x = y;
            y = z;
-         System.out.println("apres permutation x="+ x+ " et y="+y);
+         System.out.println("apres permutation x="+ x+ " et y="+y); */
+         
+     Scanner sc = new Scanner(System.in);
+     int a = 0;
+     int b = 0;
+     int x = 0;
+     System.out.print("saisir a ? : ");
+     a = sc.nextInt();
+     System.out.print("saisir b ? : ");
+     b = sc.nextInt();
+     x = (a+b)*2 ;
+     System.out.println("le resultat de (a+b)*2 est "+ x );
      
-          
+             
+  
         
                   }                }
     
